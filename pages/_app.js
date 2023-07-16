db75e25d1e2d0817d8d4c0e7c4a917940e319670
Diptansh Kumar <139675087+diptansh-1@users.noreply.github.com>
2023-07-16 17:46:47 +0530

    new commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
 import "@/styles/globals.css";
 import Navbar from "../components/Navbar";
-import { ColorProvider } from '../components/ColorContext'
+import { ColorProvider } from "../components/ColorContext";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-    <ColorProvider>
-      <div className="root-container grid md:grid-cols-[10%_90%] md:grid-rows-[10%_90%] md:gap-10 h-screen w-screen overflow-x-hidden">
-        <div className="md:col-span-1 md:row-span-2">
-          <Navbar />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <ColorProvider>
+        <div className="root-container grid md:grid-cols-[10%_90%] md:grid-rows-[10%_90%] md:gap-10 h-screen w-screen overflow-x-hidden">
+          <div className="md:col-span-1 md:row-span-2">
+            <Navbar />
+          </div>
+          <Component {...pageProps} />
         </div>
-        <Component {...pageProps} />
-      </div>
       </ColorProvider>
     </>
   );
