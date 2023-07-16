380bea9e89e794f4a8724b55dd9f3dc36ffd029b
Diptansh Kumar <139675087+diptansh-1@users.noreply.github.com>
2023-07-16 17:27:07 +0530

    new commit

--- components/addnote.jsx
+++ components/addnote.jsx
@@ -43,7 +43,7 @@ const Addnote = () => {
       <div
         className={`h-52 w-72 ${
           phonecolorpallet ? "flex" : "hidden"
-        } justify-center items-center fixed self-end justify-self-end mr-16 -mb-9 gap-x-2 md:hidden`}
+        } justify-center items-center sticky self-end justify-self-end mr-16 -mb-9 gap-x-2 md:hidden`}
       >
         <Link href="/Notes/addnotes">
           <motion.div
