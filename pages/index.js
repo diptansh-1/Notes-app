9021d3043ba4e9855874d28414903d0a7d3aabe7
Diptansh Kumar <139675087+diptansh-1@users.noreply.github.com>
2023-07-27 13:47:04 +0530

    title and icon

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import Notes from '../components/notes'
 import Addnote from "../components/addnote";
+import Head from 'next/head';
 
 
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Notes App</title>
+      <link rel='icon' href='/Logo.png'/>
+    </Head>
     <Notes/>
     <Addnote />
 
